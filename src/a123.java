f70a7d081b2110cde062494c65545cb8a08ef0cc
106022022 <106022022@live.asia.edu.tw>
2017-10-02 16:05:06 +0800

    123

---
+++ src/a123.java
@@ -0,0 +1,22 @@
+public class a123 {
+    public static void main(String[] args)  {
+        //  顯示訊息
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        System.out.println("請輸入華氏  ");
+        int f= sc.nextInt();
+
+        int s = f * 9/5+32;
+        System.out.println("華氏:" + f);
+        System.out.println("攝氏:" + s);
+
+
+
+
+
+    }
+
+
+
+
+}
---
+++ src/km.java
@@ -0,0 +1,19 @@
+public class km {
+    public static void main(String[] args) {
+        //  顯示訊息
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        System.out.println("請輸入英里  ");
+        int mi = sc.nextInt();
+
+        double km = mi * 1.6;
+        System.out.println("英里:" + mi);
+        System.out.println("公里:" + km);
+
+
+    }
+
+    }
+
+
+
