138c801afff2425d724c6919c834fd5f192ba28d
I432 <106022022@live.asia.edu.tw>
2017-10-16 14:58:22 +0800

    a123

---
+++ src/M12345.java
@@ -0,0 +1,23 @@
+public class M12345 {
+
+   public static void main(String[]args){
+       //顯示訊息
+       java.util.Scanner sc =new java.util.Scanner(System.in);
+
+       String str=sc.next();
+       char symbol=sc.next().charAt(0);
+
+       String newStr=str.replace(symbol,'\n');
+       System.out.println(newStr);
+
+
+   }
+
+
+
+
+
+}
+
+
+
